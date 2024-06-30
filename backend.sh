59a29a9ba7531dac0906fcb0e91f2b7ec59b1ecd
vijayganisetti <ganisettisai@gmail.com>
2024-06-30 16:13:52 +0530

    first

--- backend.sh
+++ backend.sh
@@ -77,7 +77,7 @@ VALIDATE $? "Enabling backend"
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h db.daws78s.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h db.vijayganisetti.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
